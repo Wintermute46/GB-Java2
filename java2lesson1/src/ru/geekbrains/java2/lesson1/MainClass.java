500f8b5c5dd924198910275edcfc439dbc4bcbb8
Wintermute46 <r.tzymbalyuk@yandex.ru>
2018-01-25 09:18:11 +0300

    GeekBrains/Java_2

--- java2lesson1/src/ru/geekbrains/java2/lesson1/MainClass.java
+++ java2lesson1/src/ru/geekbrains/java2/lesson1/MainClass.java
@@ -1,25 +1,28 @@
 package ru.geekbrains.java2.lesson1;
 
 import ru.geekbrains.java2.lesson1.competitors.*;
-import ru.geekbrains.java2.lesson1.obstacles.Cross;
-import ru.geekbrains.java2.lesson1.obstacles.Obstacle;
-import ru.geekbrains.java2.lesson1.obstacles.Wall;
-import ru.geekbrains.java2.lesson1.obstacles.Water;
+import ru.geekbrains.java2.lesson1.obstacles.*;
 
 public class MainClass {
     public static void main(String[] args) {
-        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
-        Obstacle[] course = {new Cross(400), new Wall(8), new Water(10)};
+//        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
+        Obstacle[] courses = {new Cross(400), new Wall(8), new Water(10)};
+//
+//        for (Competitor a: competitors) {
+//            for(Obstacle o : course){
+//                o.doIt(a);
+//                if (!a.isOnDistance()) break;
+//            }
+//        }
 
-        for (Competitor a: competitors) {
-            for(Obstacle o : course){
-                o.doIt(a);
-                if (!a.isOnDistance()) break;
-            }
-        }
+        Course course = new Course(courses);
+        Team team = new Team("Спартак", new Human("Вася"), new Human("Петя"), new Human("Вова"), new Human("Коля"));
+        course.doIt(team);
+
         System.out.println("=======================");
-        for (Competitor a: competitors){
-            a.showResult();
-        }
+//        for (Competitor a: competitors){
+//            a.showResult();
+//        }
+        team.showResults();
     }
 }
